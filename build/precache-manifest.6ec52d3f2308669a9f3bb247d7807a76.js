2cf214dc9a2ed96ea81d52dd8b0b75593da7a5f3
szkapec <szkapec999@o2.pl>
2020-02-17 22:12:28 +0100

    add

---
+++ build/precache-manifest.6ec52d3f2308669a9f3bb247d7807a76.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "dea45d61d7e3867d5d7e",
+    "url": "/react/static/js/main.16b654bb.chunk.js"
+  },
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  },
+  {
+    "revision": "26584ffbdd56e2378ab0",
+    "url": "/react/static/js/2.f693184a.chunk.js"
+  },
+  {
+    "revision": "6fc1d8fe5a3f17e73f1ecd0d757475d1",
+    "url": "/react/static/media/link.6fc1d8fe.svg"
+  },
+  {
+    "revision": "af737b5505b4cdb4a433cc3ac1f234af",
+    "url": "/react/static/media/bulb.af737b55.svg"
+  },
+  {
+    "revision": "0969c92b498df082b2f18fc455b2fa13",
+    "url": "/react/static/media/logout.0969c92b.svg"
+  },
+  {
+    "revision": "84b70f8a7155a0edb0576db0efc9ace2",
+    "url": "/react/static/media/pen.84b70f8a.svg"
+  },
+  {
+    "revision": "051af8ba4d1984fc498ff6da805311cb",
+    "url": "/react/static/media/twitter.051af8ba.svg"
+  },
+  {
+    "revision": "a14461b86078261a696a425d13ffe612",
+    "url": "/react/static/media/magnifier.a14461b8.svg"
+  },
+  {
+    "revision": "e43ce6c85c8383cb4442972a422cf804",
+    "url": "/react/static/media/plus.e43ce6c8.svg"
+  },
+  {
+    "revision": "7d0a43fe0b94dee2933d131344e5c978",
+    "url": "/react/index.html"
+  }
+];
